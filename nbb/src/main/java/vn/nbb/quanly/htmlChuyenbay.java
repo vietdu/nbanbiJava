9f15df932e0d71a441e042045a1e191a06b36b71
viet du <du.nguyenviet@hcmut.edu.vn>
2021-06-03 22:37:44 +0700

    hoan thanh viec thanh toan

--- nbb/src/main/java/vn/nbb/quanly/htmlChuyenbay.java
+++ nbb/src/main/java/vn/nbb/quanly/htmlChuyenbay.java
@@ -7,10 +7,14 @@ import java.sql.ResultSet;
 import nv.nbb.connectdata;
 
 public class htmlChuyenbay {
+	private int i;
 	private String htmlBay="";
 	private String duongbay;
-	public htmlChuyenbay(String duongbay) {
+	private String jQbay1="";
+	private String jQbay2="";
+	public htmlChuyenbay(String duongbay,int i) {
 		this.duongbay=duongbay;
+		this.i=i;
 	}
 	public String getHtmlBay() {
 	
@@ -34,31 +38,40 @@ public class htmlChuyenbay {
 			String tgbay=rs.getString(7);
 			String anh = rs.getString(8);
 			System.out.println(anh);
-			htmlBay = htmlBay+"<br>"+"        <div value='"+id+"' class='grid wide container thequan'>\r\n"
-					+ "                                                    <div class='prolayer-fly' >\r\n"
-					+ "                                                   <div class='prolayer3'>              \r\n"
+			
+			jQbay1 =jQbay1+"\r\n click"+id+"=document.getElementById('"+id+"'),";
+			jQbay2=jQbay2+"   \r\n click"+id+".addEventListener('click', (event)=>{\r\n"
+					+"  const result2 = document.querySelector('.resulttt');\r\n"
+					+ "  result2.textContent = `${event.target.value}`;\r\n"
+					+ "      hienthithanhtoan.classList.add('thanhtoanshow')\r\n"
+					+ "   })";
+			
+			
+			htmlBay = htmlBay+"<br>"+"        <div value='"+gia+"' id='"+id+"' class='grid wide container thequan' style='cursor: pointer;'>\r\n"
+					+ "                                                    <div value='"+gia+"' class='prolayer-fly' >\r\n"
+					+ "                                                   <div value='"+gia+"' class='prolayer3'>              \r\n"
 					+ "                                                   <img src='"+anh+"' width='40' height='10' alt=''>\r\n"
 					+ "                                                   </div>\r\n"
-					+ "                                                   <div class='block-content'>\r\n"
-					+ "                                                   <div class='prolay-full' style='display: flex;'>\r\n"
-					+ "                                                   <div class='prolayze'  id='bay5' title='8:40 AM on SAT, MAY 29 '>\r\n"
+					+ "                                                   <div value='"+gia+"' class='block-content'>\r\n"
+					+ "                                                   <div value='"+gia+"' class='prolay-full' style='display: flex;'>\r\n"
+					+ "                                                   <div value='"+gia+"' class='prolayze'  title='8:40 AM on SAT, MAY 29 '>\r\n"
 					+ "                                                      <b>"+batdau+"</b><br>\r\n"
 					+ "                                                      <p>"+tenhang+"</p>\r\n"
 					+ "                                                   </div>\r\n"
-					+ "                                                   <div class='list-fly'>\r\n"
+					+ "                                                   <div value='"+gia+"' class='list-fly'>\r\n"
 					+ "                                                      <h6 style='color: #79b18a;'>"+tgbay+"</h6>\r\n"
 					+ "                                                      <p>"+tendb+"</p>\r\n"
 					+ "                                                   </div>\r\n"
-					+ "                                                   <div class='list-skyline'>\r\n"
+					+ "                                                   <div value='"+gia+"' class='list-skyline'>\r\n"
 					+ "                                                      <h6 style='color: #79b18a;'>Nonstop</h6>\r\n"
 					+ "                                                   </div>\r\n"
-					+ "                                                   <div class='list-money'>\r\n"
+					+ "                                                   <div value='"+gia+"' class='list-money'>\r\n"
 					+ "                                                      <h6 style='color: #79b18a;'>"+gia+"</h6>\r\n"
 					+ "                                                      <p>round trip</p>\r\n"
 					+ "                                                   </div>\r\n"
 					+ "                                                   </div>\r\n"
-					+ "                                                    <div class='content-sp' style='display: none;'>\r\n"
-					+ "                                                <div class='content'>\r\n"
+					+ "                                                    <div  value='"+gia+"' class='content-sp' style='display: none;'>\r\n"
+					+ "                                                <div value='"+gia+"' class='content'>\r\n"
 					+ "                                                          <p>SGN-DAD</p>\r\n"
 					+ "                                                          <p>round trip</p>\r\n"
 					+ "                                                </div>\r\n"
@@ -81,7 +94,16 @@ public class htmlChuyenbay {
 	{
 		e.printStackTrace();
 	}
+	if(i==0)
+	{
 	return htmlBay;
+	}
+	else if(i==1) {
+		return jQbay1;
+	}
+	else {
+		return jQbay2;
+	}
 	
 	}
 }
